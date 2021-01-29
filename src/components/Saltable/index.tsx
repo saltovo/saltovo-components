ca2d0406fb3234c2282c0dcea4f90fae06be581d
luyi <luyi5@shanghai-electric.com>
2021-01-29 15:08:38 +0800

    fix: 未引入antd的tree组件的css

--- src/components/Saltable/index.tsx
+++ src/components/Saltable/index.tsx
@@ -4,6 +4,7 @@ import Saltable from './Saltable';
 import { TableProps } from 'antd/lib/table';
 import { Columnsvalue } from './container';
 import { VList } from './vlist';
+import 'antd/lib/tree/style/index.css';
 
 export interface SaltableProps extends TableProps<any> {
   toolBarRender?: any;
